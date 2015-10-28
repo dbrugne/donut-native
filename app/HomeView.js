f374e93fac90fda1a4941d76d4cbb94a7da78a10
dbrugne <damien.brugne@gmail.com>
2015-10-28 16:47:29 +0100

    Comment obsolete

--- app/HomeView.js
+++ app/HomeView.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var React = require('react-native');
-var Button = require('react-native-button');
 var {
   AppRegistry,
   AsyncStorage,
@@ -62,16 +61,17 @@ class HomeView extends Component {
       return this.renderLoadingView();
     }
 
+//    <View style={styles.links}>
+//      <TouchableHighlight onPress={(this.onLogout.bind(this))} style={styles.button}>
+//        <Text style={styles.logout}>Logout</Text>
+//      </TouchableHighlight>
+//      <TouchableHighlight onPress={(this.onSocketMe.bind(this))} style={styles.button}>
+//        <Text style={styles.socketme}>SocketMe</Text>
+//      </TouchableHighlight>
+//    </View>
+
     return (
       <View style={styles.main}>
-        <View style={styles.links}>
-          <TouchableHighlight onPress={(this.onLogout.bind(this))} style={styles.button}>
-            <Text style={styles.logout}>Logout</Text>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={(this.onSocketMe.bind(this))} style={styles.button}>
-            <Text style={styles.socketme}>SocketMe</Text>
-          </TouchableHighlight>
-        </View>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this.renderRoom}
@@ -95,29 +95,27 @@ class HomeView extends Component {
       </View>
     );
   }
-  onLogout() {
-    var that = this;
-    AsyncStorage.multiRemove(['token', 'code'], function () {
-      var login = that.props.navigator.getCurrentRoutes().filter(function (element) {
-        return (element.name === 'login');
-      })[0];
-      if (login) {
-        that.props.navigator.popToRoute(login);
-      }
-    });
-  }
-  onSocketMe() {
-    client.roomMessage('557ed3a4bcb50bc52b74744b', 'test depuis react-native', null, null, function (res) {
-      console.log(res);
-    });
-  }
+//  onLogout() {
+//    var that = this;
+//    AsyncStorage.multiRemove(['token', 'code'], function () {
+//      var login = that.props.navigator.getCurrentRoutes().filter(function (element) {
+//        return (element.name === 'login');
+//      })[0];
+//      if (login) {
+//        that.props.navigator.popToRoute(login);
+//      }
+//    });
+//  }
+//  onSocketMe() {
+//    client.roomMessage('557ed3a4bcb50bc52b74744b', 'test depuis react-native', null, null, function (res) {
+//      console.log(res);
+//    });
+//  }
 };
 
 var styles = StyleSheet.create({
   main: {
     marginTop: 25,
-    flexDirection: 'column',
-    flexWrap: 'nowrap'
   },
   links: {
     flex: 1,
