cf84264d4cf2d0488a922bb614db656b1aba6543
Sebastien Pariaud <sebastienpariaud@gmail.com>
2015-12-15 16:44:49 +0100

    update group page
    #21

--- app/views/GroupAskMembership.js
+++ app/views/GroupAskMembership.js
@@ -8,8 +8,24 @@ var {
   Component,
   StyleSheet
 } = React;
+var {
+  Icon
+} = require('react-native-icons');
 
+var _ = require('underscore');
 var client = require('../libs/client');
+var ListItem = require('../elements/ListItem');
+var s = require('../styles/style');
+
+var i18next = require('../libs/i18next');
+i18next.addResourceBundle('en', 'local', {
+  'message': 'Members have special priviledges such as direct access to certain private donuts and to other members of this community.',
+  'from': 'Message from ',
+  'request-title': 'I request membership',
+  'password-title': 'I have the password',
+  'email-title': 'I have an authorized e-mail',
+  'other-request': 'You can join this community only if invited by its moderators.'
+});
 
 class GroupAskMembership extends Component {
 
@@ -20,7 +36,7 @@ class GroupAskMembership extends Component {
       success: false,
       error: null
     };
-    this.data = null;
+    this.data = {};
     this.id = props.id;
   }
   componentDidMount () {
@@ -35,16 +51,16 @@ class GroupAskMembership extends Component {
       });
     }
     if (!response.success) {
+      this.data = response;
       this.setState({
         loading: false
       });
-      this.data = response
     } else {
+      this.data = response;
       this.setState({
         loading: false,
         success: true
       });
-      this.data = response
     }
   }
   render () {
@@ -64,25 +80,106 @@ class GroupAskMembership extends Component {
       );
     }
     if (!this.state.success) {
-      console.log('dfgdfdfd');
+      var disclaimer = null;
+      // @todo replace <View style={s.alertWarning}> by element "alert" create is not exist
+      if (this.data.options.disclaimer) {
+        disclaimer = (
+          <View style={s.alertWarning}>
+            <Text>{i18next.t('local:from')} @{this.data.options.owner_username}:</Text>
+            <View style={{flexDirection: 'row'}}>
+              <Icon
+                name='fontawesome|quote-right'
+                size={14}
+                color='#666'
+                style={{width: 14, height: 14, marginRight: 2}}
+                />
+              <Text>{this.data.options.disclaimer}</Text>
+            </View>
+          </View>
+        );
+      }
       return (
         <View>
-          {this.renderDisclaimer.bind(this)}
+          <View style={styles.container}>
+            <Text style={styles.message}>{i18next.t('local:message')}</Text>
+            {disclaimer}
+          </View>
+          <View style={styles.containerOptions}>
+            {this.renderListOptions()}
+          </View>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <Text>Vous êtes déjà membre!</Text>
         </View>
       );
     }
   }
-
-  renderDisclaimer () {
-    console.log('ddffdf');
-    if (this.data.disclaimer) {
+  renderListOptions () {
+    var nbrOptions = (this.data.options.allowed_domains ? 1 : 0) + (this.data.options.password ? 1 : 0) + (this.data.options.request ? 1 : 0);
+    if (nbrOptions > 1) {
+      var request = null;
+      var password = null;
+      var email = null;
+      if (this.data.options.request) {
+        request = (
+          <ListItem
+            text={i18next.t('local:request-title')}
+            first={true}
+            action='true'
+            type='button'
+            />
+        );
+      }
+      if (this.data.options.password) {
+        password = (
+          <ListItem
+            text={i18next.t('local:password-title')}
+            first={(!this.data.options.request)}
+            last={(!this.data.options.email)}
+            action='true'
+            type='button'
+            />
+        );
+      }
+      if (this.data.options.allowed_domains) {
+        email = (
+          <ListItem
+            text={i18next.t('local:email-title')}
+            last={true}
+            action='true'
+            type='button'
+            />
+        );
+      }
       return (
         <View>
-          <Text>{this.data.disclaimer}</Text>
+          {request}
+          {password}
+          {email}
         </View>
       );
+    } else if (nbrOptions === 1) {
+        if (this.data.options.request) {
+          return (
+            <View><Text>one option request</Text></View>
+          );
+        } else if (this.data.options.password) {
+          return (
+            <View><Text>one option password</Text></View>
+          );
+        } else {
+          return (
+            <View><Text>one option email</Text></View>
+          );
+        }
+    } else {
+      return (
+        <View><Text>{i18next.t('local:other-request')}</Text></View>
+      );
     }
-    return null;
   }
 }
 
@@ -101,6 +198,18 @@ var styles = StyleSheet.create({
   },
   loading: {
     height: 120
+  },
+  container: {
+    flex: 1,
+    marginTop: 10,
+    backgroundColor: '#FFF'
+  },
+  containerOptions: {
+    marginTop: 50
+  },
+  message: {
+    color: '#424242',
+    fontSize: 16
   }
 });
 
