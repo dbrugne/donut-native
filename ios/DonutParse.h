b73f98db550cc58c82c27d255c979cfa7fed344d
dbrugne <damien.brugne@gmail.com>
2016-01-04 14:13:19 +0100

    Fix XCODE
    #61

---
+++ ios/DonutParse.h
@@ -0,0 +1,13 @@
+//
+//  DonutParse.h
+//  donutMobile
+//
+//  Created by Damien Brugne on 01/01/2016.
+//  Copyright © 2016 DONUT SAS. All rights reserved.
+//
+
+#ifndef DonutParse_h
+#define DonutParse_h
+
+
+#endif /* DonutParse_h */
