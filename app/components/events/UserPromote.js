7f80d5c1a4a7e135cc5dded7a2923022c800f2b9
Yoann Fuks <yoannfuks@hotmail.fr>
2016-01-28 17:32:18 +0100

    fix user:ban user:deban render in discussion field
    #163

--- app/components/events/UserPromote.js
+++ app/components/events/UserPromote.js
@@ -10,19 +10,33 @@ var s = require('../../styles/events');
 
 var i18next = require('../../libs/i18next');
 i18next.addResourceBundle('en', 'userPromote', {
-  'action': 'has been __what__ by '
+  'action': ' has been __what__ by '
 });
 
 module.exports = React.createClass({
-  render () {
+  propTypes: {
+    type: React.PropTypes.string,
+    data: React.PropTypes.object,
+    navigator: React.PropTypes.object
+  },
+  render: function () {
+    var what;
+    switch (this.props.type) {
+      case 'user:ban':
+        what = 'banned'; break;
+      case 'user:deban':
+        what = 'unbanned'; break;
+      default :
+        what = this.props.type;
+    }
     return (
-      <View style={[s.statusBlock, s.event]}>
+      <View style={[s.statusBlock, s.event, {marginLeft: 25}]}>
         <Username
           user_id={this.props.data.to_user_id}
           username={this.props.data.to_username}
           navigator={this.props.navigator}
         />
-        <Text>{i18next.t('userPromote:action', { what: this.props.type })}</Text>
+        <Text>{i18next.t('userPromote:action', { what: what })}</Text>
         <Username
           user_id={this.props.data.user_id}
           username={this.props.data.username}
