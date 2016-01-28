b792b42c3096a4ea80bebb05561f522b810217c5
Yoann Fuks <yoannfuks@hotmail.fr>
2016-01-28 16:35:34 +0100

    upgrade android to match react native 0.19.0-rc
    #160

--- android/app/src/main/java/com/donutmobile/MainActivity.java
+++ android/app/src/main/java/com/donutmobile/MainActivity.java
@@ -1,27 +1,22 @@
 package me.donut.mobile;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
 
 // @CodePush
 import com.microsoft.codepush.react.CodePush;
-import android.support.v4.app.FragmentActivity;
-
-import com.facebook.react.LifecycleState;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 // to retrieve config
 import java.lang.reflect.Field;
 
 // @Icons
 import com.smixx.reactnativeicons.ReactNativeIcons;
-import java.util.Arrays;
-import com.smixx.reactnativeicons.IconFont;
 
 // @FacebookLogin
 import com.magus.fblogin.FacebookLoginPackage;
@@ -38,154 +33,121 @@ import com.notificationandroid.NotificationAndroidPackage;
 import com.parse.ParseInstallation;
 import com.parsepushnotification.ParsePushNotificationPackage;
 
-import android.util.Log;
-
-public class MainActivity extends FragmentActivity implements DefaultHardwareBackBtnHandler {
-
-    private ReactInstanceManager mReactInstanceManager;
-    private ReactRootView mReactRootView;
-
-    // @FacebookLogin
-    private FacebookLoginPackage mFacebookLoginPackage;
+public class MainActivity extends ReactActivity {
 
     // @ImagePicker
     private ImagePickerPackage mImagePicker;
 
-    // @Parse
-    private NotificationAndroidPackage mNotificationAndroid;
-    private ParsePushNotificationPackage mParsePushNotification;
+    // @Codepush
+    private CodePush _codePush;
 
+    /**
+     * We override onCreate for parse installation
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mReactRootView = new ReactRootView(this);
+      super.onCreate(savedInstanceState);
 
-        // Per-env configuration
-        String env = "none";
-        Object buildType = getBuildConfigValue("BUILD_TYPE");
-        Object ServerIp = getBuildConfigValue("LOCAL_IP");
-        Object Version = getBuildConfigValue("DONUT_VERSION");
-        Bundle b = new Bundle();
-        if (buildType == null) {
-            b.putString("DONUT_BUILD", "null");
-        } else {
-            b.putString("DONUT_BUILD", buildType.toString());
-        }
-        if (buildType != null && buildType.toString() == "release") {
-            b.putString("DONUT_ENVIRONMENT", "production");
-            env = "production";
-        } else {
-            b.putString("DONUT_ENVIRONMENT", "test");
-            env = "test";
-        }
-        if (Version == null) {
-            b.putString("DONUT_VERSION", "null");
-        } else {
-            b.putString("DONUT_VERSION", Version.toString());
-        }
-        if (ServerIp == null) {
-            b.putString("REACT_SERVER_ADDRESS", "null");
-        } else {
-            b.putString("REACT_SERVER_ADDRESS", ServerIp.toString());
-        }
-
-        // @CodePush
-        String codePushKey;
-        if (env == "production") {
-          codePushKey = "QUin28f78rg5cJaf-Ao3bF2APvsyVJgNW_EDe";
-        } else {
-          codePushKey = "F83XLkIqywhmOu-2Mi-HzsamPn8jVJgNW_EDe";
-        }
-        CodePush codePush = new CodePush(codePushKey, this);
-
-        // @FacebookLogin
-        mFacebookLoginPackage = new FacebookLoginPackage();
-
-        // @ImagePicker
-        mImagePicker = new ImagePickerPackage(this);
-
-        // @Parse
-        mNotificationAndroid = new NotificationAndroidPackage(this);
-        mParsePushNotification = new ParsePushNotificationPackage(this);
-
-        mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-
-                // @CodePush
-                //.setBundleAssetName("index.android.bundle")
-                .setJSBundleFile(codePush.getBundleUrl("index.android.bundle"))
-
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-
-                // @CodePush
-                .addPackage(codePush.getReactPackage())
-
-                // @Icons
-                .addPackage(new ReactNativeIcons())
-
-                 // @FacebookLogin
-                .addPackage(mFacebookLoginPackage)
-
-                // @ImagePicker
-                .addPackage(mImagePicker)
-
-                // @Parse
-                .addPackage(mNotificationAndroid)
-                .addPackage(mParsePushNotification)
-
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
-
-        // register in parse installation with env field
-        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
-        installation.put("env", env);
-        installation.saveInBackground();
-        mReactRootView.startReactApplication(mReactInstanceManager, "donutMobile", b);
-
-        setContentView(mReactRootView);
-    }
-
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-            mReactInstanceManager.showDevOptionsDialog();
-            return true;
-        }
-        return super.onKeyUp(keyCode, event);
-    }
-
-    @Override
-    public void onBackPressed() {
-      if (mReactInstanceManager != null) {
-        mReactInstanceManager.onBackPressed();
+      String env = "none";
+      Object buildType = getBuildConfigValue("BUILD_TYPE");
+      if (buildType != null && buildType.toString() == "release") {
+          env = "production";
       } else {
-        super.onBackPressed();
+          env = "test";
       }
+
+      // register in parse installation with env field
+      ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+      installation.put("env", env);
+      installation.saveInBackground();
     }
 
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
     @Override
-    public void invokeDefaultOnBackPressed() {
-      super.onBackPressed();
+    protected String getMainComponentName() {
+        return "donutMobile";
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onPause();
-        }
+    /**
+     * Returns the launchOptions which will be passed to the {@link ReactInstanceManager}
+     * when the application is started. By default, this will return null and an empty
+     * object will be passed to your top level component as its initial props.
+     * If your React Native application requires props set outside of JS, override
+     * this method to return the Android.os.Bundle of your desired initial props.
+     */
+     @Override
+    protected Bundle getLaunchOptions() {
+      Object buildType = getBuildConfigValue("BUILD_TYPE");
+      Object ServerIp = getBuildConfigValue("LOCAL_IP");
+      Object Version = getBuildConfigValue("DONUT_VERSION");
+      String env = "none";
+      Bundle b = new Bundle();
+      if (buildType == null) {
+          b.putString("DONUT_BUILD", "null");
+      } else {
+          b.putString("DONUT_BUILD", buildType.toString());
+      }
+      if (buildType != null && buildType.toString() == "release") {
+          b.putString("DONUT_ENVIRONMENT", "production");
+          env = "production";
+      } else {
+          b.putString("DONUT_ENVIRONMENT", "test");
+          env = "test";
+      }
+      if (Version == null) {
+          b.putString("DONUT_VERSION", "null");
+      } else {
+          b.putString("DONUT_VERSION", Version.toString());
+      }
+      if (ServerIp == null) {
+          b.putString("REACT_SERVER_ADDRESS", "null");
+      } else {
+          b.putString("REACT_SERVER_ADDRESS", ServerIp.toString());
+      }
+      return b;
     }
 
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
 
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this, this);
-        }
+   /**
+   * A list of packages used by the app. If the app uses additional views
+   * or modules besides the default ones, add more packages here.
+   */
+    @Override
+    protected List<ReactPackage> getPackages() {
+      Object buildType = getBuildConfigValue("BUILD_TYPE");
+
+      // @CodePush
+      String codePushKey;
+      if (buildType != null && buildType.toString() == "release") {
+          codePushKey = "QUin28f78rg5cJaf-Ao3bF2APvsyVJgNW_EDe";
+      } else {
+          codePushKey = "F83XLkIqywhmOu-2Mi-HzsamPn8jVJgNW_EDe";
+      }
+      this._codePush = new CodePush(codePushKey, this);
+
+      // @ImagePicker
+      mImagePicker = new ImagePickerPackage(this);
+
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        this._codePush.getReactPackage(),
+        new FacebookLoginPackage(),
+        new NotificationAndroidPackage(this),
+        new ParsePushNotificationPackage(this),
+        new ReactNativeIcons(),
+        mImagePicker
+      );
     }
 
     @Override
@@ -194,6 +156,12 @@ public class MainActivity extends FragmentActivity implements DefaultHardwareBac
 
         // @ImagePicker
         mImagePicker.handleActivityResult(requestCode, resultCode, data);
+    }
+
+    //@Codepush
+    @Override
+    protected String getJSBundleFile() {
+        return this._codePush.getBundleUrl("index.android.bundle");
     }
 
     private static Object getBuildConfigValue(String fieldName) {
