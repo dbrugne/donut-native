8426d8a6ca8da8d1075396b558fc85a4c96fb0dc
Yann <y.lastapis@gmail.com>
2016-03-09 15:03:59 +0100

    #266

--- app/views/GroupAskRequest.js
+++ app/views/GroupAskRequest.js
@@ -14,6 +14,15 @@ var Disclaimer = require('../components/Disclaimer');
 var Button = require('../components/Button');
 var KeyboardAwareComponent = require('../components/KeyboardAware');
 var i18next = require('../libs/i18next');
+i18next.addResourceBundle('en', 'GroupAskRequest', {
+  'allowed-pending': 'Access to this community is by invitation, and you already have a request in progress.',
+  'placeholder-request': 'Your motivations, background ...',
+  'info-request': 'Your contact details and your following message (optional) will be sent to the moderators of this community.',
+  'send': 'SEND',
+  'success-request': 'Request sent ! You will be notified when accepted.',
+  'message-wrong-format': 'The message should be less than 200 character',
+  'not-confirmed': 'Action authorized to verified accounts only. Verify your e-mail.'
+});
 
 var GroupAskRequest = React.createClass({
   propTypes: {
@@ -33,7 +42,7 @@ var GroupAskRequest = React.createClass({
     let content = null;
     if (this.props.isAllowedPending) {
       content = (
-          <Text style={{ margin: 20, fontFamily: 'Open Sans', fontSize: 14, color: '#F15261' }}>{i18next.t('group.allowed-pending')}</Text>
+          <Text style={{ margin: 20, fontFamily: 'Open Sans', fontSize: 14, color: '#F15261' }}>{i18next.t('GroupAskRequest:allowed-pending')}</Text>
       );
     } else {
       content = (
@@ -44,7 +53,7 @@ var GroupAskRequest = React.createClass({
             <View style={{width: 1, height: 40, backgroundColor: '#FC2063'}}/>
             <TextInput autoCapitalize='none'
                        ref='input'
-                       placeholder={i18next.t('group.placeholder-request')}
+                       placeholder={i18next.t('GroupAskRequest:placeholder-request')}
                        onChangeText={(text) => this.setState({motivations: text})}
                        value={this.state.request}
                        style={{ flex: 1, fontFamily: 'Open Sans', fontSize: 14, fontWeight: '400', color: '#333', height: 40, paddingVertical: 8, paddingHorizontal: 10 }}
@@ -53,10 +62,10 @@ var GroupAskRequest = React.createClass({
           <View style={{ flex: 1 }}/>
           <View style={{ height: 1, alignSelf: 'stretch', backgroundColor: '#E7ECF3', marginBottom: 10 }}/>
           <Text
-            style={{ alignSelf: 'stretch', marginHorizontal: 20, fontFamily: 'Open Sans', fontSize: 12, color: '#AFBAC8', fontStyle: 'italic' }}>{i18next.t('group.info-request')}</Text>
+            style={{ alignSelf: 'stretch', marginHorizontal: 20, fontFamily: 'Open Sans', fontSize: 12, color: '#AFBAC8', fontStyle: 'italic' }}>{i18next.t('GroupAskRequest:info-request')}</Text>
           <Button type='gray'
                   onPress={this.onSendRequest}
-                  label={i18next.t('group.send')}
+                  label={i18next.t('GroupAskRequest:send')}
                   loading={this.state.loading}
                   style={{ alignSelf: 'stretch', marginTop: 10, marginHorizontal: 20 }}
             />
@@ -86,12 +95,12 @@ var GroupAskRequest = React.createClass({
     app.client.groupRequest(this.state.data.group_id, this.state.motivations, (response) => {
       this.setState({loading: false});
       if (response.success) {
-        alert.show(i18next.t('group.success-request'));
+        alert.show(i18next.t('GroupAskRequest:success-request'));
         app.trigger('refreshGroup', true);
         this.props.navigator.popToTop();
       } else {
         if (response.err === 'allow-pending' || response.err === 'message-wrong-format' || response.err === 'not-confirmed') {
-          return alert.show(i18next.t('group.' + response.err));
+          return alert.show(i18next.t('GroupAskRequest:' + response.err));
         }
         return alert.show(i18next.t('messages.' + response.err));
       }
