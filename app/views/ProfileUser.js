2d9ad5cabb5d7ded415e8d8e88a34d3c8a99fd7c
Yann <y.lastapis@gmail.com>
2016-03-11 10:54:09 +0100

    Move User Header to another View

--- app/views/ProfileUser.js
+++ app/views/ProfileUser.js
@@ -5,19 +5,16 @@ var {
   StyleSheet,
   View,
   Text,
-  Component,
-  Image,
-  TouchableHighlight,
   ScrollView
-} = React;
+  } = React;
 
 var _ = require('underscore');
-var common = require('@dbrugne/donut-common/mobile');
 var app = require('../libs/app');
 var s = require('../styles/style');
 var date = require('../libs/date');
 var hyperlink = require('../libs/hyperlink');
 var ListItem = require('../components/ListItem');
+var UserHeader = require('./UserHeader');
 var currentUser = require('../models/current-user');
 var navigation = require('../navigation/index');
 
@@ -27,18 +24,19 @@ i18next.addResourceBundle('en', 'ProfileUser', {
   'unlock': 'unblock this user',
   'lock': 'block this user',
   'blocked': 'this user blocked you',
-  'discuss': 'CHAT',
   'report': 'report user'
 });
 
-class UserProfileView extends Component {
-  constructor (props) {
-    super(props);
-
-    this.state = {
+var UserProfileView = React.createClass({
+  propTypes: {
+    data: React.PropTypes.object.isRequired,
+    navigator: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
       banned: this.props.data.banned
     };
-  }
+  },
   render () {
     var bio = _.unescape(this.props.data.bio);
 
@@ -52,47 +50,43 @@ class UserProfileView extends Component {
     var location = null;
     if (this.props.data.location) {
       location = (
-      <ListItem
-        text={_.unescape(this.props.data.location)}
-        type='text'
-        first
-        imageLeft={require('../assets/icon-location.png')}
-        />
+        <ListItem
+          text={_.unescape(this.props.data.location)}
+          type='text'
+          first
+          imageLeft={require('../assets/icon-location.png')}
+          />
       );
     }
 
     var website = null;
     if (this.props.data.website) {
       website = (
-      <ListItem
-        text={this.props.data.website.title}
-        type='edit-button'
-        onPress={() => hyperlink.open(this.props.data.website.href)}
-        first={!this.props.data.location}
-        action
-        imageLeft={require('../assets/icon-link.png')}
-        />
+        <ListItem
+          text={this.props.data.website.title}
+          type='edit-button'
+          onPress={() => hyperlink.open(this.props.data.website.href)}
+          first={!this.props.data.location}
+          action
+          imageLeft={require('../assets/icon-link.png')}
+          />
       );
     }
 
     var registeredAt = (
-    <ListItem
-      type='text'
-      text={i18next.t('ProfileUser:registered-on', {date: date.shortDate(this.props.data.registered)})}
-      first={(!this.props.data.location && !this.props.data.website)}
-      imageLeft={require('../assets/icon-time.png')}
-      />
+      <ListItem
+        type='text'
+        text={i18next.t('ProfileUser:registered-on', {date: date.shortDate(this.props.data.registered)})}
+        first={(!this.props.data.location && !this.props.data.website)}
+        imageLeft={require('../assets/icon-time.png')}
+        />
     );
 
     return (
       <ScrollView style={styles.main}>
-        <BackgroundComponent avatar={this.props.data.avatar} >
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            {this._renderAvatar(this.props.data.avatar)}
-            {this._renderIdentifier()}
-            {isBannedLink}
-          </View>
-        </BackgroundComponent>
+        <UserHeader data={this.props.data}>
+          {isBannedLink}
+        </UserHeader>
         <View style={[s.listGroup]}>
           <Text style={styles.bio}>{bio}</Text>
           {location}
@@ -103,9 +97,8 @@ class UserProfileView extends Component {
         </View>
       </ScrollView>
     );
-  }
-
-  _renderBanned() {
+  },
+  _renderBanned () {
     if (this.props.data.user_id === currentUser.get('user_id')) {
       return;
     }
@@ -115,22 +108,20 @@ class UserProfileView extends Component {
         type='switch'
         imageLeft={require('../assets/icon-ban.png')}
         text={ this.state.banned ? i18next.t('ProfileUser:unlock') : i18next.t('ProfileUser:lock')}
-        onSwitch={this._toggleBanned.bind(this)}
+        onSwitch={this._toggleBanned}
         switchValue={this.state.banned}
         />
     );
-  }
-
-  _toggleBanned() {
+  },
+  _toggleBanned () {
     // currently banned
     if (this.state.banned) {
       this._unbanUser();
     } else {
-      this._banUser()
+      this._banUser();
     }
-  }
-
-  _renderReport() {
+  },
+  _renderReport () {
     if (this.props.data.user_id === currentUser.get('user_id')) {
       return;
     }
@@ -144,67 +135,7 @@ class UserProfileView extends Component {
         imageLeft={require('../assets/icon-ban.png')}
         />
     );
-  }
-
-  _renderDiscuss() {
-    if (this.props.data.user_id === currentUser.get('user_id')) {
-      return null;
-    }
-
-    return (
-      <Image style={{width: 152.5, height: 35, marginVertical: 20}} source={require('../assets/chat-button.png')} >
-        <TouchableHighlight
-          underlayColor='transparent'
-          style={{ flex:1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
-          onPress={() => app.trigger('joinUser', this.props.data.user_id)}
-          >
-          <Text style={{ textAlign: 'center', fontFamily: 'Open Sans', fontWeight: '600', fontSize: 14, color: '#353F4C' }}> {i18next.t('ProfileUser:discuss')} </Text>
-        </TouchableHighlight>
-      </Image>
-    );
-  }
-
-  _renderIdentifier() {
-    if (this.props.data.realname) {
-      return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 10, flexDirection: 'column'}}>
-          <View style={{backgroundColor: 'transparent', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={styles.realname}>{_.unescape(this.props.data.realname)}</Text>
-            <View style={[styles.status, this.props.data.status === 'connecting' && styles.statusConnecting, this.props.data.status === 'offline' && styles.statusOffline, this.props.data.status === 'online' && styles.statusOnline]} />
-            <Text style={styles.statusText}>{this.props.data.status}</Text>
-          </View>
-          <Text style={[styles.username, styles.usernameGray]}>@{this.props.data.username}</Text>
-          {this._renderDiscuss()}
-        </View>
-      );
-    }
-
-    return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 10, flexDirection: 'column'}}>
-      <View style={{backgroundColor: 'transparent', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={[styles.realname]}>@{this.props.data.username}</Text>
-        <View style={[styles.status, this.props.data.status === 'connecting' && styles.statusConnecting, this.props.data.status === 'offline' && styles.statusOffline, this.props.data.status === 'online' && styles.statusOnline]} />
-        <Text style={styles.statusText}>{this.props.data.status}</Text>
-      </View>
-      {this._renderDiscuss()}
-    </View>
-    );
-  }
-
-  _renderAvatar (avatar) {
-    if (!avatar) {
-      return null;
-    }
-    var avatarUrl = common.cloudinary.prepare(avatar, 150);
-    if (!avatarUrl) {
-      return null;
-    }
-
-    return (
-      <Image style={styles.avatar} source={{uri: avatarUrl}}/>
-    );
-  }
-
+  },
   _banUser () {
     app.client.userBan(this.props.data.user_id, (response) => {
       if (response.err) {
@@ -212,8 +143,7 @@ class UserProfileView extends Component {
       }
       this.setState({banned: true});
     });
-  }
-
+  },
   _unbanUser () {
     app.client.userDeban(this.props.data.user_id, (response) => {
       if (response.err) {
@@ -221,34 +151,6 @@ class UserProfileView extends Component {
       }
       this.setState({banned: false});
     });
-  }
-}
-
-var BackgroundComponent = React.createClass({
-  propTypes: {
-    children: React.PropTypes.element.isRequired,
-    avatar: React.PropTypes.string
-  },
-
-  render: function() {
-    var avatarUrl = null;
-    if (this.props.avatar) {
-      avatarUrl = common.cloudinary.prepare(this.props.avatar, 300);
-    }
-
-    if (avatarUrl) {
-      return (
-        <Image style={[styles.container, {resizeMode: 'cover'}]} source={{uri: avatarUrl}}>
-          {this.props.children}
-        </Image>
-      );
-    }
-
-    return (
-      <View style={[styles.container, {position: 'relative'}]}>
-        {this.props.children}
-      </View>
-    );
   }
 });
 
@@ -265,42 +167,7 @@ var styles = StyleSheet.create({
   avatar: {
     width: 100,
     height: 100,
-    marginTop:20
-  },
-  username: {
-    color: '#AFBAC8',
-    backgroundColor: 'transparent',
-    fontFamily: 'Open Sans',
-    fontSize: 16,
-    marginBottom: 5
-  },
-  usernameGray: {
-    color: '#b6b6b6'
-  },
-  realname: {
-    color: '#FFFFFF',
-    backgroundColor: 'transparent',
-    fontFamily: 'Open Sans',
-    fontSize: 16,
-    fontWeight: '600'
-  },
-  status: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    marginHorizontal: 5,
-    marginTop: 4
-  },
-  statusOnline: { backgroundColor: '#18C095' },
-  statusConnecting: { backgroundColor: 'rgb(255, 218, 62)' },
-  statusOffline: { backgroundColor: 'rgb(119,119,119)' },
-  statusText: {
-    color: '#FFFFFF',
-    fontStyle: 'italic',
-    fontWeight: '600',
-    fontSize: 16,
-    fontFamily: 'Open Sans',
-    backgroundColor: 'transparent'
+    marginTop: 20
   },
   icon: {
     width: 14,
---
+++ app/views/UserHeader.js
@@ -0,0 +1,209 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableHighlight
+  } = React;
+
+var _ = require('underscore');
+var common = require('@dbrugne/donut-common/mobile');
+var currentUser = require('../models/current-user');
+var app = require('../libs/app');
+var i18next = require('../libs/i18next');
+i18next.addResourceBundle('en', 'UserHeader', {
+  'discuss': 'CHAT'
+});
+
+var UserHeaderView = React.createClass({
+  defaultPropTypes: {
+    small: false
+  },
+  propTypes: {
+    model: React.PropTypes.object,
+    data: React.PropTypes.any,
+    children: React.PropTypes.any
+  },
+  getInitialState: function () {
+    return {
+      data: this.props.data ? this.props.data : this.props.model.toJSON()
+    };
+  },
+  render () {
+    return (
+      <View style={[styles.container]}>
+        <BackgroundComponent avatar={this.state.data.avatar}>
+          <View style={{alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center'}}>
+            {this._renderAvatar()}
+            {this._renderName()}
+            {this.props.children}
+          </View>
+        </BackgroundComponent>
+      </View>
+    );
+  },
+  _renderName () {
+    if (this.props.data.realname) {
+      return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 10, flexDirection: 'column'}}>
+          <View style={{backgroundColor: 'transparent', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={styles.realname}>{_.unescape(this.props.data.realname)}</Text>
+            <View style={[styles.status, this.props.data.status === 'connecting' && styles.statusConnecting, this.props.data.status === 'offline' && styles.statusOffline, this.props.data.status === 'online' && styles.statusOnline]}/>
+            <Text style={styles.statusText}>{this.props.data.status}</Text>
+          </View>
+          <Text style={[styles.username, styles.usernameGray]}>@{this.props.data.username}</Text>
+          {this._renderDiscuss()}
+        </View>
+      );
+    }
+
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 10, flexDirection: 'column'}}>
+        <View style={{backgroundColor: 'transparent', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={[styles.realname]}>@{this.props.data.username}</Text>
+          <View style={[styles.status, this.props.data.status === 'connecting' && styles.statusConnecting, this.props.data.status === 'offline' && styles.statusOffline, this.props.data.status === 'online' && styles.statusOnline]}/>
+          <Text style={styles.statusText}>{this.props.data.status}</Text>
+        </View>
+        {this._renderDiscuss()}
+      </View>
+    );
+  },
+  _renderDiscuss () {
+    if (this.props.data.user_id === currentUser.get('user_id')) {
+      return null;
+    }
+
+    return (
+      <Image style={{width: 152.5, height: 35, marginVertical: 20}} source={require('../assets/chat-button.png')}>
+        <TouchableHighlight
+          underlayColor='transparent'
+          style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
+          onPress={() => app.trigger('joinUser', this.props.data.user_id)}
+          >
+          <Text style={{ textAlign: 'center', fontFamily: 'Open Sans', fontWeight: '600', fontSize: 14, color: '#353F4C' }}> {i18next.t('UserHeader:discuss')} </Text>
+        </TouchableHighlight>
+      </Image>
+    );
+  },
+  _renderAvatar () {
+    var avatarUrl = common.cloudinary.prepare(this.state.data.avatar, 150);
+    if (!avatarUrl) {
+      return null;
+    }
+
+    return (
+      <Image style={styles.avatar} source={{uri: avatarUrl}}/>
+    );
+  }
+});
+
+var BackgroundComponent = React.createClass({
+  propTypes: {
+    children: React.PropTypes.element.isRequired,
+    avatar: React.PropTypes.string
+  },
+
+  render: function () {
+    var avatarUrl = null;
+    if (this.props.avatar) {
+      avatarUrl = common.cloudinary.prepare(this.props.avatar, 300);
+    }
+
+    // @todo implement blur here
+    if (avatarUrl) {
+      return (
+        <Image
+          style={{ resizeMode: 'cover', flexDirection: 'column', alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center' }}
+          source={{uri: avatarUrl}}>
+          <View style={{ paddingBottom: 20, flexDirection: 'column', alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0,0,0,0.50)' }}>
+            {this.props.children}
+          </View>
+        </Image>
+      );
+    }
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    alignSelf: 'stretch',
+    flexDirection: 'column'
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    marginTop: 20,
+    marginBottom: 10,
+    borderRadius: 40,
+    shadowColor: 'rgb(28,36,47)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 3,
+    shadowOpacity: 0.15
+  },
+  roomname: {
+    color: '#FFFFFF',
+    fontFamily: 'Open Sans',
+    fontSize: 18,
+    fontWeight: '600',
+    lineHeight: 18
+  },
+  group: {
+    fontFamily: 'Open Sans',
+    fontSize: 11,
+    color: '#AFBAC8',
+    letterSpacing: 0.85,
+    lineHeight: 14
+  },
+  description: {
+    fontFamily: 'Open Sans',
+    fontSize: 14,
+    color: '#394350',
+    lineHeight: 14
+  },
+  username: {
+    color: '#AFBAC8',
+    backgroundColor: 'transparent',
+    fontFamily: 'Open Sans',
+    fontSize: 16,
+    marginBottom: 5
+  },
+  usernameGray: {
+    color: '#b6b6b6'
+  },
+  realname: {
+    color: '#FFFFFF',
+    backgroundColor: 'transparent',
+    fontFamily: 'Open Sans',
+    fontSize: 16,
+    fontWeight: '600'
+  },
+  status: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    marginHorizontal: 5,
+    marginTop: 4
+  },
+  statusOnline: {backgroundColor: '#18C095'},
+  statusConnecting: {backgroundColor: 'rgb(255, 218, 62)'},
+  statusOffline: {backgroundColor: 'rgb(119,119,119)'},
+  statusText: {
+    color: '#FFFFFF',
+    fontStyle: 'italic',
+    fontWeight: '600',
+    fontSize: 16,
+    fontFamily: 'Open Sans',
+    backgroundColor: 'transparent'
+  }
+});
+
+module.exports = UserHeaderView;
